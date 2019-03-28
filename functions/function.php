6d7b4ec8bab9ef4d6f4b8d123907f7877448120c
Druvo <zahidhasan.druvo.100@gmail.com>
2019-03-28 17:04:03 +0600

    first

---
+++ functions/function.php
@@ -0,0 +1,9 @@
+<?php
+   require_once('config.php');
+   function get_header(){
+      require_once('include/header.php');
+   }
+   function get_footer(){
+      require_once('include/footer.php');
+   }
+ ?>
