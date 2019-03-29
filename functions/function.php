75e8c44fd50da124181e60abe4aa373c2f969c32
Druvo <zahidhasan.druvo.100@gmail.com>
2019-03-29 11:50:55 +0600

    all delete
    asdf

--- functions/function.php
+++
@@ -1,9 +0,0 @@
-<?php
-   require_once('config.php');
-   function get_header(){
-      require_once('include/header.php');
-   }
-   function get_footer(){
-      require_once('include/footer.php');
-   }
- ?>
